fa01606a726c0a89aa24e3c4652f7c2057575d46
Ramya V <ramya@Ramyas-MacBook-Air.local>
2022-01-06 22:16:22 +0530

    coercion

---
+++ concepts_code/index.js
@@ -0,0 +1,23 @@
+const adder = true + 5 
+// 6
+
+const adder = false + 5 
+// 5
+
+const adder = '5' + 5 
+// 55
+
+const adder = '5' - 5 
+// 0
+
+const adder = true + 5 
+// 6
+
+// coercion is a technique where js interprets and tries to do smthng for you
+
+// values that interpret as false:
+// false
+// 0
+// null
+// undefined
+// ''
