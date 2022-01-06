1f01ad9c22dbf09ed9347fd3fe4f803e1bbbee94
Ramya V <ramya@Ramyas-MacBook-Air.local>
2022-01-06 21:28:24 +0530

    changes

--- concepts_code/index.js
+++ concepts_code/index.js
@@ -1,27 +1,2 @@
-// currying - we define a mthod, make more functions out of it
-// we curry our functions by presetting some argument values
-
-// 2 ways - bind and closures
-
-// bind
-
-let multiply = function(X, Y){
-  console.log(X*Y)
-}
-
-let multiplyBy2 = multiply.bind(this, 2);
-multiplyBy2(4)
-
-let multiplyBy3 = multiply.bind(this, 3);
-multiplyBy3(6)
-
-// closures
-
-let mul = function(x){
-  return function(y){
-    console.log(x*y)
-  }
-}
-
-let mul2 = mul(2)
-mul2(5)
+array = [1,2,3,4]
+console.log(array.length)
