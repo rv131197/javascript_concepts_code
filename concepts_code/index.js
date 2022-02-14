0b2eb4ec707abe596a512ae57ee0e346cb869425
Ramya V <ramya@Ramyas-MacBook-Air.local>
2022-02-15 00:44:27 +0530

    code'

--- concepts_code/index.js
+++ concepts_code/index.js
@@ -1,2 +1,8 @@
-array = [1,2,3,4]
-console.log(array.length)
+function multiply(x){
+    return function multiplyBy(y){
+        console.log(x*y)
+    }
+}
+
+const multiplyBy2 = multiply(3)
+multiplyBy2(2)
